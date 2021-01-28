8c418e5548afd0858ccad505618b1fa55af10272
semuserable <semuserable@gmail.com>
2021-01-28 11:46:31 +0200

    initial

---
+++ Runtime/OnStartHello.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class OnStartHello : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+        => Debug.Log($"Hello from {nameof(OnStartHello)}.Start()!");
+}
