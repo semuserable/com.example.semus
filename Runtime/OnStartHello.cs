43a56f3ed99b211d4d23d7f9461f192a0a099207
semuserable <semuserable@gmail.com>
2021-02-03 10:43:41 +0200

    OnStartHello removed

--- Runtime/OnStartHello.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-public class OnStartHello : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-        => Debug.Log($"Hello from {nameof(OnStartHello)}.Start()!");
-}
